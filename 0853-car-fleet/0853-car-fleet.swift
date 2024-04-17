0967d8cde4067afd32211410041078ce4b369ae6
Sarathi <sarathi.kannan363@gmail.com>
2024-04-17 18:03:05 +0530

    Time: 475 ms (8.00%), Space: 21.8 MB (73.33%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.swift
+++ 0853-car-fleet/0853-car-fleet.swift
@@ -1,13 +1,13 @@
 class Solution {
     func carFleet(_ target: Int, _ position: [Int], _ speed: [Int]) -> Int {
+        var pair = zip(position, speed).sorted{ $0 > $1}
         var stack: [Double] = []
-        var pair = zip(position, speed).sorted{$0 > $1}
         for (pos, s) in pair {
-            let hoursTake: Double = Double(Double(target - pos) / Double(s))
-            stack.append(hoursTake)
-            guard stack.count >= 2 else {continue}
-            let suffixStack = stack.suffix(2)
-            if suffixStack.last! <= suffixStack.first! {
+            let value: Double = Double(Double(target - pos) / Double(s))
+            stack.append(value)
+            guard stack.count >= 2 else { continue }
+            let suffixArr: [Double] = stack.suffix(2)
+            if suffixArr.last! <= suffixArr.first! {
                 stack.removeLast()
             }
         }
