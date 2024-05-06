96a539fb17eef813633cab1f7c95de36c78728fe
Sarathi <sarathi.kannan363@gmail.com>
2024-05-06 14:18:49 +0530

    Time: 5 ms (81.87%), Space: 15.7 MB (96.89%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.swift
+++ 0022-generate-parentheses/0022-generate-parentheses.swift
@@ -1,19 +1,20 @@
 class Solution {
     func generateParenthesis(_ n: Int) -> [String] {
-        var stackStr: [String] = []
-        func generate(left: Int, right: Int, string: String) {
+        var stack: [String] = []
+        func dfs(_ left: Int, _ right: Int, _ string: String) {
             if right == n {
-                stackStr.append(string)
+                stack.append(string)
                 return
             }
+            
             if left < n {
-                generate(left: left+1, right: right, string: string + "(")
+                dfs(left + 1, right, string + "(")
             }
             if right < left {
-                generate(left: left, right: right+1, string: string + ")")
+                dfs(left, right + 1, string + ")")
             }
         }
-        generate(left: 1, right: 0, string: "(")
-        return stackStr
+        dfs(1, 0, "(")
+        return stack
     }
 }
