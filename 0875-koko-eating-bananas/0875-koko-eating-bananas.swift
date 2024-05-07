a60ea4df3bd3ba32f22b8e591426d471a6e4d553
Sarathi <sarathi.kannan363@gmail.com>
2024-05-07 14:15:00 +0530

    Time: 101 ms (27.39%), Space: 15.5 MB (82.91%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+        var result: Int = piles.max()!
+        var left: Int = 1
+        var right: Int = piles.max()!
+        while left <= right {
+            let mid = (left + right) / 2
+            var totalTime:Int = 0
+            for pile in piles {
+                totalTime += Int(ceil(Double(pile) / Double(mid)))
+            }
+            if totalTime <= h {
+                result = min(result, mid)
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return result
+    }
+}
