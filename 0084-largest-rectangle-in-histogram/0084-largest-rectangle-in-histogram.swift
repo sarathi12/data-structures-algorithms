909db0c65db38b927d007c3b0ec098e98333db36
Sarathi <sarathi.kannan363@gmail.com>
2024-05-09 18:22:33 +0530

    Time: 324 ms (6.82%), Space: 20.1 MB (11.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.swift
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.swift
@@ -1,27 +1,27 @@
 class Solution {
     func largestRectangleArea(_ heights: [Int]) -> Int {
-        var maxArea: Int = 0
-        typealias indexHeights = (index: Int, height: Int)
-        var stack: [indexHeights] = []
+        var largest: Int = 0
+        typealias IndexHeight = (index:Int, height:Int)
+        var stack: [IndexHeight] = []
         for (index, height) in heights.enumerated() {
             var start = index
             while !stack.isEmpty, stack.last!.height > height {
                 let lastVal = stack.removeLast()
-                var newHeight = lastVal.height * (index - lastVal.index)
-                if newHeight > maxArea {
-                    maxArea = newHeight
+                let maxHeight = lastVal.height * (index - lastVal.index)
+                if maxHeight > largest {
+                    largest = maxHeight
                 }
                 start = lastVal.index
-            }            
+            }
             stack.append((index: start, height: height))
         }
         while !stack.isEmpty {
             let lastVal = stack.removeLast()
-            var newHeight = lastVal.height * (heights.count - lastVal.index)
-                if newHeight > maxArea {
-                    maxArea = newHeight
-                }
+            let maxHeight = lastVal.height * (heights.count - lastVal.index)
+            if maxHeight > largest {
+                largest = maxHeight
+            }
         }
-        return maxArea
+        return largest
     }
 }
