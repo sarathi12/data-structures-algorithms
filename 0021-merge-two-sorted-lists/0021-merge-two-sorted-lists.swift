7b441c0efe5e8d3ea6ec1ec9eeb364f23ff8fbc7
Sarathi <sarathi.kannan363@gmail.com>
2024-05-29 18:01:50 +0530

    Time: 6 ms (71.90%), Space: 15.4 MB (75.85%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        var dummy: ListNode? = ListNode()
+        var sortedList: ListNode? = dummy
+        
+        var l1 = list1
+        var l2 = list2
+        while l1 != nil && l2 != nil {
+            if (l1?.val ?? 0) <= (l2?.val ?? 0) {
+                sortedList?.next = l1
+                l1 = l1?.next
+            } else {
+                sortedList?.next = l2
+                l2 = l2?.next
+            }
+            sortedList = sortedList?.next
+        }
+        
+        while l1 != nil {
+            sortedList?.next = l1
+            l1 = l1?.next
+            sortedList = sortedList?.next
+        }
+        while l2 != nil {
+            sortedList?.next = l2
+            l2 = l2?.next
+            sortedList = sortedList?.next
+        }
+        return dummy?.next
+    }
+}
