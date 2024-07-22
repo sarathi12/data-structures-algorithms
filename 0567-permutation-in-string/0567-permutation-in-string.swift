1812c27c6d38ecaed076c44004cd033620795349
Sarathi <sarathi.kannan363@gmail.com>
2024-07-22 08:06:27 +0530

    Time: 28 ms (46.96%), Space: 16.5 MB (43.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.swift
+++ 0567-permutation-in-string/0567-permutation-in-string.swift
@@ -1,41 +1,37 @@
 class Solution {
     func checkInclusion(_ s1: String, _ s2: String) -> Bool {
         guard s2.count >= s1.count else { return false}
-        if s1 == s2 {
-            return true
-        }
-        
-        let s1 = Array(s1)
-        let s2 = Array(s2)
+        var s1 = Array(s1)
+        var s2 = Array(s2)
         var countS1: [Character: Int] = [:]
         var countS2: [Character: Int] = [:]
-        var left: Int = 0
         for char in s1 {
             countS1[char, default: 0] += 1
         }
         var keep: Int = countS1.count
         var need: Int = 0
+        var left: Int = 0
         for right in 0..<s2.count {
-            let char = s2[right]
-            countS2[char, default: 0] += 1
-            if let countS1 = countS1[char], let countS2 = countS2[char], countS1 == countS2 {
+            let charS2 = s2[right]
+            countS2[charS2, default: 0] += 1
+            if let countCharS1 = countS1[charS2], let countCharS2 = countS2[charS2], countCharS1 == countCharS2 {
                 need += 1
             }
-            
-            while need == keep {
-                let diff = (right - left) + 1
-                if diff == s1.count {
-                    return true
-                }
-                let leftChar = s2[left]
-                left += 1
-                countS2[leftChar, default: 0] -= 1 
-                if let countS1 = countS1[leftChar], let countS2 = countS2[leftChar], countS1 > countS2 {
-                    need -= 1
-                }
+            if need == keep {
+                return true
             }
+            let distance = (right - left) + 1
+            if distance == s1.count {
+                let charLeft = s2[left]
+                if let countCharS1 = countS1[charLeft], let countCharS2 = countS2[charLeft], countCharS1 == countCharS2 {
+                  need -= 1
+                }
+                countS2[charLeft, default: 0] -= 1
+                left += 1
+            }
+
         }
-        
         return false
+
     }
 }
