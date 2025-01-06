198925d93975ea0a418aa74ce33c7146402a81ee
Sarathi <sarathi.kannan363@gmail.com>
2025-01-06 18:32:36 +0530

    Time: 21 ms (35.06%), Space: 17.4 MB (88.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.swift
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.swift
@@ -1,6 +1,7 @@
 class Solution {
     func subarraySum(_ nums: [Int], _ k: Int) -> Int {
         var totalCount: Int = 0
+        /*
         for i in 0..<nums.count {
             
             var newVal = nums[i]
@@ -14,6 +15,15 @@ class Solution {
                    totalCount += 1
                 }
             }
+        }
+        */
+        var sum: Int = 0
+        var prefixSum: [Int: Int] = [0:1]
+        for num in nums {
+            sum += num
+            var diff = sum - k
+            totalCount += prefixSum[diff] ?? 0
+            prefixSum[sum] = 1 + (prefixSum[sum] ?? 0)
         }
 
         return totalCount
