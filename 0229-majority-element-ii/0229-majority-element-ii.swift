0c3ff4befef2ce0bd444aa179af906a213effdb1
Sarathi <sarathi.kannan363@gmail.com>
2025-01-07 20:28:55 +0530

    Time: 0 ms (100%), Space: 18.6 MB (10.29%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.swift
+++ 0229-majority-element-ii/0229-majority-element-ii.swift
@@ -1,16 +1,26 @@
 class Solution {
     func majorityElement(_ nums: [Int]) -> [Int] {
         var targetCount: Int = nums.count / 3
-        var newArray: [Int] = []
+        var newArray: [Int] = nums.sorted()
         var countArray: [Int: Int] = [:]
-        for num in nums {
-            countArray[num, default: 0] += 1
-        } 
-        for (num, count) in countArray {
-            if count > targetCount {
-                newArray.append(num)
+        var result: [Int] = []
+        var i = 0
+        while i < newArray.count {
+            var j = i + 1
+            var count = 1
+            while j < newArray.count {
+                if newArray[i] == newArray[j] {
+                   count += 1
+                   j += 1
+                } else {
+                    break
+                }   
             }
+            if count > targetCount {
+                result.append(newArray[i])
+            }
+            i = j
         }
-        return newArray
+        return result
     }
 }
